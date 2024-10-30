2ffa380bf08383f63f0620de9f7bbd61f5a4c22c
ayoubelmenssouri <130702810+ayoubelmenssouri@users.noreply.github.com>
2024-10-30 11:43:27 +0100

    changement inoutssd

--- test/test_add.js
+++ test/test_add.js
@@ -3,7 +3,7 @@ var calculate = require("../app");
 
 describe("Test add", () => {
   it("should return 2 when the values are 1 and 1", () => {
-    assert.equal(calculate.add(1,1), 2);
+    assert.equal(calculate.add(1,1), 4);
   });
   it("should return 5 when the values are 2 and 3", () => {
     assert.equal(calculate.add(2,3), 5);
