9672779e3008533d4a900e2f082f71a0d748ebf9
ayoubelmenssouri <130702810+ayoubelmenssouri@users.noreply.github.com>
2024-10-30 11:46:15 +0100

    changement inoutssdsdsasds

--- test/test_sub.js
+++ test/test_sub.js
@@ -3,7 +3,7 @@ var calculate = require("../app");
 
 describe("Test sub", () => {
   it("should return 0 when the values are 1 and 1", () => {
-    assert.equal(calculate.sub(1, 1), 11);
+    assert.equal(calculate.sub(1, 1), 0);
   });
   it("should return -1 when the values are 2 and 3", () => {
     assert.equal(calculate.sub(2, 3), -1);
